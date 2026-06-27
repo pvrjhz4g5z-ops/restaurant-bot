1985cfa6100e701c9c650a5ee94787d752dec386
pvrjhz4g5z-ops <pvrjhz4g5z@privaterelay.appleid.com>
2026-06-28 00:20:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,8 +141,7 @@ async def cancel_booking(callback: types.CallbackQuery):
 
 async def main():
     await db.init_db()
-    await dp.start_polling(bot)
-
-
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)    
 if __name__ == "__main__":
     asyncio.run(main())
